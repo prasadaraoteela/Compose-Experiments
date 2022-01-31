6d7a517b1611d44647b31cc24d0b88f0161ac7ea
prasadaraoteela <prasadaraoteela@gmail.com>
2022-01-31 18:31:05 +0530

    Added retrofit module

---
+++ library/retrofit/src/main/java/com/technobabble/retrofit/di/RetrofitModule.kt
@@ -0,0 +1,34 @@
+package com.technobabble.retrofit.di
+
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by Prasada Rao on 31/01/22 6:20 PM
+ **/
+@Module
+object RetrofitModule {
+
+  @Singleton
+  @Provides
+  fun provideRetrofit(httpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+    .baseUrl("https://www.themealdb.com/api/json/v1/1/")
+    .client(httpClient)
+    .addConverterFactory(GsonConverterFactory.create(Gson()))
+    .build()
+
+  @Provides
+  fun provideOkHttpClient(): OkHttpClient {
+    val httpClientBuilder = OkHttpClient.Builder()
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    httpClientBuilder.addInterceptor(httpLoggingInterceptor)
+    return httpClientBuilder.build()
+  }
+}
