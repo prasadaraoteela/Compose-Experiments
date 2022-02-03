45338aac46226d1e23a076c5d82fcd395f8f1e20
prasadaraoteela <prasadaraoteela@gmail.com>
2022-02-03 12:35:52 +0530

    Added animation to meal category picture

--- mealzapp/src/main/java/com/udemy/mealz/ui/details/MealCategoryDetailScreen.kt
+++ mealzapp/src/main/java/com/udemy/mealz/ui/details/MealCategoryDetailScreen.kt
@@ -1,19 +1,27 @@
 package com.udemy.mealz.ui.details
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
@@ -35,29 +43,68 @@ fun MealCategoryDetailScreen(mealCategoryId: String) {
     return
   }
 
+  var pictureState by remember { mutableStateOf(MealCategoryPictureState.Normal) }
+
+  val transition = updateTransition(targetState = pictureState, label = mealCategoryId)
+  val imageSize: Dp by transition.animateDp(
+    targetValueByState = { state -> state.size },
+    label = "MealCategoryPictureSizeAnimation"
+  )
+  val imageBorderWidth: Dp by transition.animateDp(
+    targetValueByState = { state -> state.borderWidth },
+    label = "MealCategoryPictureBorderWidthAnimation"
+  )
+  val imageBorderColor: Color by transition.animateColor(
+    targetValueByState = { state -> state.color },
+    label = "MealCategoryPictureBorderAnimation"
+  )
+
   Column {
-    Image(
-      painter = rememberImagePainter(
-        data = mealCategory!!.thumbnailUrl,
-      ),
-      contentDescription = mealCategory!!.category,
-      modifier = Modifier.fillMaxWidth(),
-      contentScale = ContentScale.Crop
-    )
-    Text(
-      text = mealCategory!!.category,
-      style = MaterialTheme.typography.h4,
-      modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
-    )
-    Text(
-      text = mealCategory!!.description,
-      style = MaterialTheme.typography.subtitle1,
-      modifier = Modifier.padding(start = 16.dp, end = 16.dp)
-    )
-    Button(onClick = { /*TODO*/ }) {
+    Row {
+      Card(
+        modifier = Modifier.padding(16.dp),
+        shape = CircleShape,
+        border = BorderStroke(width = imageBorderWidth, color = imageBorderColor)
+      ) {
+        Image(
+          painter = rememberImagePainter(
+            data = mealCategory!!.thumbnailUrl,
+          ),
+          contentDescription = mealCategory!!.category,
+          modifier = Modifier.size(imageSize),
+          contentScale = ContentScale.Crop
+        )
+      }
+      Column {
+        Text(
+          text = mealCategory!!.category,
+          style = MaterialTheme.typography.h4,
+          modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
+        )
+        Text(
+          text = mealCategory!!.description,
+          style = MaterialTheme.typography.subtitle1,
+          modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+      }
+    }
+    Button(onClick = {
+      pictureState = if (pictureState == MealCategoryPictureState.Normal) {
+        MealCategoryPictureState.Expanded
+      } else {
+        MealCategoryPictureState.Normal
+      }
+    }) {
       Text(text = stringResource(R.string.button_change_meal_state_image))
     }
   }
+}
+
+enum class MealCategoryPictureState(
+  val color: Color, val size: Dp, val borderWidth: Dp
+) {
+  Normal(color = Color.Magenta, size = 120.dp, borderWidth = 8.dp),
+  Expanded(color = Color.Green, size = 200.dp, borderWidth = 24.dp)
 }
 
 @Preview(showBackground = true)
